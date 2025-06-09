3787fea76368f4b8505179c0f51cc2069b7f3882
최수빈 <lightsaber2929@gmail.com>
2025-06-09 11:43:44 +0900

    investing.com 지표 발표 일정 크롤링

---
+++ utils/calendar.py
@@ -0,0 +1,141 @@
+import urllib
+import urllib.request
+from urllib.error import HTTPError
+from urllib.parse import urlencode
+
+from bs4 import BeautifulSoup
+import datetime
+import arrow
+
+
+class Good():
+	def __init__(self):
+		self.value = "+"
+		self.name = "good"
+
+	def __repr__(self):
+		return "<Good(value='%s')>" % (self.value)
+
+
+class Bad():
+	def __init__(self):
+		self.value = "-"
+		self.name = "bad"
+
+	def __repr__(self):
+		return "<Bad(value='%s')>" % (self.value)
+
+
+class Unknow():
+	def __init__(self):
+		self.value = "?"
+		self.name = "unknow"
+
+	def __repr__(self):
+		return "<Unknow(value='%s')>" % (self.value)		
+
+
+class Investing():
+	def __init__(self, date_from=None, date_to=None):
+		base_url = 'https://www.investing.com/economic-calendar/'
+		params = {}
+		if date_from and date_to:
+			params['dateFrom'] = date_from
+			params['dateTo'] = date_to
+			self.uri = f"{base_url}?{urlencode(params)}"
+		else:
+			self.uri = base_url
+		self.req = urllib.request.Request(self.uri)
+		self.req.add_header('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36')
+		self.result = []
+
+	def news(self):
+		try:
+			response = urllib.request.urlopen(self.req)
+			html = response.read()
+			soup = BeautifulSoup(html, "html.parser")
+
+			table = soup.find('table', {"id": "economicCalendarData"})
+			tbody = table.find('tbody')
+			rows = tbody.findAll('tr', {"class": "js-event-item"})
+
+			for row in rows:
+				news = {
+					'timestamp': None,
+					'country': None,
+					'impact': None,
+					'url': None,
+					'name': None,
+					'bold': None,
+					'fore': None,
+					'prev': None,
+					'signal': None,
+					'type': None
+				}
+
+				_datetime = row.attrs['data-event-datetime']
+				news['timestamp'] = arrow.get(_datetime, "YYYY/MM/DD HH:mm:ss").timestamp
+
+				cols = row.find('td', {"class": "flagCur"})
+				flag = cols.find('span')
+				news['country'] = flag.get('title')
+
+				impact = row.find('td', {"class": "sentiment"})
+				bull = impact.findAll('i', {"class": "grayFullBullishIcon"})
+				news['impact'] = len(bull)
+
+				event = row.find('td', {"class": "event"})
+				a = event.find('a')
+				news['url'] = "{}{}".format(self.uri, a['href'])
+				news['name'] = a.text.strip()
+
+				# 이벤트 타입 판별
+				if event.find('span', {"class": "smallGrayReport"}):
+					news['type'] = "report"
+				elif event.find('span', {"class": "audioIconNew"}):
+					news['type'] = "speech"
+				elif event.find('span', {"class": "smallGrayP"}):
+					news['type'] = "release"
+				elif event.find('span', {"class": "sandClock"}):
+					news['type'] = "retrieving data"
+
+				bold = row.find('td', {"class": "bold"})
+				news['bold'] = bold.text.strip() if bold.text != '' else ''
+
+				fore = row.find('td', {"class": "fore"})
+				news['fore'] = fore.text.strip()
+
+				prev = row.find('td', {"class": "prev"})
+				news['prev'] = prev.text.strip()
+
+				if "blackFont" in bold['class']:
+					news['signal'] = Unknow()
+				elif "redFont" in bold['class']:
+					news['signal'] = Bad()
+				elif "greenFont" in bold['class']:
+					news['signal'] = Good()
+				else:
+					news['signal'] = Unknow()
+
+				self.result.append(news)
+
+		except HTTPError as error:
+			print("Oops... Get error HTTP {}".format(error.code))
+
+		return self.result
+
+
+if __name__ == "__main__":
+	# 한 달치 데이터
+	i = Investing(date_from="2024-06-01", date_to="2024-06-30")
+	news_data = i.news()
+	print(*news_data, sep='\n')
+
+	# CSV로 저장
+	import csv
+	if news_data:
+		keys = news_data[0].keys()
+		with open('calendar_data.csv', 'w', newline='', encoding='utf-8') as f:
+			writer = csv.DictWriter(f, fieldnames=keys)
+			writer.writeheader()
+			writer.writerows(news_data)
